b8837e65bb3f677e319604762a794971af86dcf2
shinjilonely <ampremshin2@gmail.com>
2026-02-10 16:09:23 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,23 @@
+{
+  "name": "anime-streaming-site",
+  "version": "1.0.0",
+  "description": "Situs web streaming anime dengan teknik scraping.",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "keywords": ["anime", "streaming", "scraping"],
+  "author": "",
+  "license": "MIT",
+  "dependencies": {
+    "express": "^4.18.2",
+    "axios": "^1.6.0",
+    "cheerio": "^1.0.0-rc.12",
+    "cors": "^2.8.5",
+    "dotenv": "^16.3.1"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1"
+  }
+      }
